e5b8071fc66c941b9b167444bb9df270f8d47f3c
mariaguti30 <mariaguti30@gmail.com>
2024-05-15 11:45:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Specify the file path
+        string filePath = "hello.txt";
+
+        // Write "Hello, World!" to the file
+        File.WriteAllText(filePath, "Hello, World!");
+    }
+}
